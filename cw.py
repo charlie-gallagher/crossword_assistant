54fa19641744109229ded98f445d66a46c644482
Charlie Debian <charlesjgallagher15@gmail.com>
2021-01-29 12:39:29 -0500

    Improve error and warning checking

--- cw.py
+++ cw.py
@@ -10,7 +10,7 @@ class CwSyntaxError(Exception):
             self.message = "No message given"
 
     def __str__(self):
-        return f'Error: {self.message}'
+        return f'{self.message}'
 
 class CwValueError(Exception):
     def __init__(self, message):
@@ -20,7 +20,7 @@ class CwValueError(Exception):
             self.message = "No message given"
 
     def __str__(self):
-        return f'Error: {self.message}'
+        return f'{self.message}'
 
 # Parse conditions
 def is_length(arg_str):
@@ -51,11 +51,19 @@ def cw_parse_cl(args):
         raise CwSyntaxError("No arguments given")
     for a in args[1:]:
         if is_length(a):
+            if out['length'] != 0:
+                raise CwSyntaxError("More than one length specified")
             out["length"] = int(a)
         elif is_letter(a):
+            pos = int(a[0])
+            cw_is_duplicate_position(pos, out["positions"])
+
             out["positions"].append(int(a[0]))
             out["letters"].append(a[1])
         elif is_expr(a):
+            pos = int(a[0])
+            cw_is_duplicate_position(pos, out["positions"])
+
             out["positions"].append(int(a[0]))
             out["letters"].append(a[1:])
         else:
@@ -67,13 +75,21 @@ def cw_parse_cl(args):
 
 
 # TODO: Make this more restrictive
+#       - Check for duplicate positions (e.g. 3e 3f)
 def cw_check_cl(cl_dict):
     # Highest letter <= length of word
     l = cl_dict['length']
     pos = cl_dict['positions']
 
     if  l < max(pos):
-        raise CwValueError(f"Letter implies word longer than length ({cl_dict['length']})")
+        max_letter = cl_dict['letters'][pos.index(max(pos))]
+        raise CwValueError(f"Letter ({max_letter}) implies word longer than length ({cl_dict['length']})")
+
+def cw_is_duplicate_position(l, position_list):
+    if l in position_list:
+        raise CwSyntaxError(f"Position ({l}) has already been used.")
+    else:
+        return(0)
 
 # Generate regular expression from parsed commands
 def cw_gen_regex(letters, positions, length):
@@ -99,9 +115,6 @@ def cw_search_words(reg_expression, filename):
                 print(m.string, end="")
             else:
                 pass
-
-
-
 
 
 
